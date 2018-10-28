2c28a6a9569afe8a6f226c747fdef878f27905cc
MonteiroIgor <contato.igormonteiro@gmail.com>
2018-10-28 17:15:45 -0300

    Classes ajustadas

---
+++ projeto_materia_oo/src/br/com/projeto/Projeto.java
@@ -0,0 +1,31 @@
+package br.com.projeto;
+
+public class Projeto {
+	
+	private String tema;
+	private String disciplina;
+	private String integrantes;
+	
+	
+	public String getTema() {
+		return tema;
+	}
+	public void setTema(String tema) {
+		this.tema = tema;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+	public String getIntegrantes() {
+		return integrantes;
+	}
+	public void setIntegrantes(String integrantes) {
+		this.integrantes = integrantes;
+	}
+	
+	
+	
+}
